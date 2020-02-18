b399eda36132fec5f0b93c416dd657954f6bd5f8
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2020-02-18 09:49:00 +0530

    final version 1.0 commit

--- js/script.js
+++ js/script.js
@@ -11,5 +11,45 @@ $(document).ready(function() {
     else{
       $(".nav-menu").removeClass("custom-navbar");
     }
+  });
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position >= 650) {
+      $(".camera-img").addClass("fromLeft");
+      $(".camera-txt").addClass("fromRight");
+    }
+    else {
+      $(".camera-img").removeClass("fromLeft");
+      $(".camera-txt").removeClass("fromRight");
+    }
+  });
+
+  $(".gallery-list-item").click(function() {
+    let value = $(this).attr("data-filter");
+    if(value === "all"){
+      $(".filter").show(300)
+    }else{
+      $(".filter").not("." + value).hide(300);
+      $(".filter").filter("." + value).show(300);
+    }
+  });
+
+  $(".gallery-list-item").click(function() {
+    $(this).addClass("active-item").siblings().removeClass("active-item");
+  })
+
+  $(window).scroll(function(){
+    let position = $(this).scrollTop();
+    if (position>=4300) {
+      $(".card-1").addClass("moveFromLeft");
+      $(".card-2").addClass("moveFromBottom");
+      $(".card-3").addClass("moveFromRight");
+    }
+    else{
+      $(".card-1").removeClassClass("moveFromLeft");
+      $(".card-2").removeClassClass("moveFromBottom");
+      $(".card-3").removeClassClass("moveFromRight");
+    }
   })
 });
